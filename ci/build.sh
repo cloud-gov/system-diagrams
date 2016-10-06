69d0949e7ccfc0f9a0e312006db08048dc1af0e7
Aidan Feldman <aidan.feldman@gsa.gov>
2016-10-06 02:30:15 -0400

    copy in ci/ directory from cg-docs
    as of
    https://github.com/18F/cg-docs/tree/da142e3ca55ce4b4a8b1841e2ffc225b6a97f6ed.

---
+++ ci/build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# install Hugo
+HUGO_VERSION=0.16
+cd /tmp/
+curl -O -L https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux-64bit.tgz
+tar xzf hugo_${HUGO_VERSION}_linux-64bit.tgz
+mv hugo /usr/bin/hugo
+cd -
+
+npm install
+npm run build
+
+# build the site
+hugo
+
+# copy files to output directory, so that they can be read by subsequent step
+if [ -n "$COPY_OUTPUT" ]; then
+  cp -R . ../cg-docs-compiled
+fi
