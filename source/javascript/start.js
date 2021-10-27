d78214e80159e7fe8c50493c57ef86819cdbaa28
Mark Headd <mark.headd@gsa.gov>
2021-10-27 16:05:45 -0400

    Move call to mermaid.version() to enable event binding

--- source/javascript/start.js
+++ source/javascript/start.js
@@ -1,23 +1,24 @@
 import mermaid from 'mermaid';
 import * as svgPng from 'save-svg-as-png';
 
-console.log( 'mermaid', mermaid.version() );
-
 // TODO This is very ugly, but for now it renders. This SVG creation should be
 // done in the build step.
 let ss = document.getElementById( 'js-stage' );
 
 let btn = document.getElementById( 'save-png-button' );
+
 btn.addEventListener('click', function() {
   let svgList = ss.getElementsByTagName('svg');
   let options = {
-    scale: 2,
+    scale: 5,
     backgroundColor: '#ffffff',
   };
   if (svgList.length) {
     svgPng.saveSvgAsPng(svgList[0], document.title+'.png', options);
   }
 });
+
+console.log( 'mermaid', mermaid.version() );
 
 mermaid.mermaidAPI.render(
   'js-stage',
